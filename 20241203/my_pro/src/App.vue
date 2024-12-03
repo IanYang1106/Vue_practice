8de6b62a3c6d73ac278c7b49510ae8706bdb7954
IanYang1106 <monoyen2001@gmail.com>
2024-12-03 23:38:30 +0800

    add 20241203 of counter

---
+++ 20241203/my_pro/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed, watch } from 'vue'
+
+// 定義響應式變量 count
+const count = ref(0)
+
+const xxx = ref('haha')
+
+// 定義增减方法
+const plus = () => {
+  count.value++ // 增加 count 的值
+}
+
+const minus = () => {
+  if (count.value > 0) {
+    // 防止 count 小於 0
+    count.value--
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="minus">-</button>
+    <input type="number" v-model="count" />
+    <button @click="plus">+</button>
+  </div>
+</template>
+
+<style scoped></style>
